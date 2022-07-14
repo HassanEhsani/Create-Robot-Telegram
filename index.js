a79f3abd94f0ec30a191aab4713f441c3cfdce20
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2022-07-14 15:36:15 +0300

    we can use telegram and telegraf function

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf("5578827276:AAG5FGWbVPPR8DoAoG9lPvMauLn7RELwbCo");
 
-bot.start(ctx => ctx.reply("😎"));
+bot.start((ctx) => {
+//     console.log(ctx.message.from)
+//   return ctx.reply("Right Now rain is stop"); this is TELEFRAF
+    console.log(ctx.message.chat);
+    return ctx.telegram.sendMessage(ctx.chat.id,"hi")
+});
 
 bot.launch();
-
-
-
