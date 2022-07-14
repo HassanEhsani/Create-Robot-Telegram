6f142e0378af1c60e97e7e34c27901c3c32c2686
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2022-07-14 18:50:15 +0300

    i add by (on) for check the all of messages

--- index.js
+++ index.js
@@ -6,19 +6,32 @@ bot.start((ctx) => {
   //     console.log(ctx.message.from)
   //   return ctx.reply("Right Now rain is stop"); this is TELEFRAF
   console.log(ctx.message.chat);
-  return ctx.telegram.sendMessage(ctx.chat.id, `سلام
+  return ctx.telegram.sendMessage(
+    ctx.chat.id,
+    `سلام
   من یک ربات فروشگاهی هستم
-  برای دیدن محصولات می تونید روی لینک زیر کلیک کنید.
+  لیست محصولات را می توانید از دکمه پایین ببینید.
   /products
-  برای تنظیمات هم می توانید روی این دکمه کلیک کنید.
+  
   /settings
   
-  `);
+  `
+  );
 });
 
-bot.command(["products","Products","محصولات"],ctx=>{
-    ctx.reply("لیست محصولات را الان برایت فرستادم")
-})
+bot.command(["products", "Products", "محصولات"], (ctx) => {
+  ctx.reply("لیست محصولات را الان برایت فرستادم");
+});
+
+bot.hears(/^محصول/, (ctx) => {
+  ctx.reply("محصولات ارایشی");
+});
+// bot.command(["yes","Yes"],ctx=>{
+//     ctx.reply("خوبه ادامه می دیم")
+// });
+// bot.command(["no","No"],ctx=>{
+//     ctx.reply("پس ادامه نمی دیم")
+// });
 
 bot.settings((ctx) => {
   ctx.reply("تنظیمات");
@@ -26,6 +39,14 @@ bot.settings((ctx) => {
 
 bot.help((ctx) => {
   ctx.reply("راهنما");
+});
+
+bot.on("text", (ctx) => ctx.reply("سلام شما یک متن فرستادید"));
+bot.on("voice", (ctx) => ctx.reply("سلام شما یک صدا فرستادید"));
+bot.on("edited_message", (ctx) => {
+
+  console.log(ctx)
+  return ctx.reply("سلام شما ویرایش کردید");
 });
 
 bot.launch();
