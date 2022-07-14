d7d59f5c487ba47922b85a31074c3704397c0d57
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2022-07-14 14:29:33 +0300

    create button start

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf("5578827276:AAG5FGWbVPPR8DoAoG9lPvMauLn7RELwbCo");
 
-bot.start(ctx => ctx.reply("I'm a robot"));
+bot.start(ctx => ctx.reply("😎"));
 
 bot.launch();
+
+
+
